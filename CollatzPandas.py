c0be3c92cf45a2035707aac344476d8423550d23
josiah <josiahwhite@gmail.com>
2019-08-11 11:19:22 -0400

    Creating dataframes and a few methods.

--- CollatzPandas.py
+++ CollatzPandas.py
@@ -11,27 +11,56 @@ from RemainderPair import RemainderPair
 
 class CollatzPandas(CollatzContainer):
 
+    """
+    DataFrame Structure
+    EVEN
+    Even, Odd, Completed, Alpha
+
+    REMAINDER
+    remainder, even, sequence, beta, num_initial_increases, num_trailing_decreases, sequence_center
+
+    REMAINDER PAIR
+    pair, even, shifted_core
+    """
+
     def __init__(self):
         super().__init__()
+        self.even = pd.DataFrame(columns=['odd',
+                                          'completed',
+                                          'alpha',
+                                          ],
+                                 index=pd.Index([], name='even'))
+        self.remainder = pd.DataFrame(columns=['even',
+                                               'sequence',
+                                               'beta',
+                                               'num_initial_increases',
+                                               'num_trailing_decreases',
+                                               'sequence_center',
+                                               ],
+                                      index=pd.Index([], name='remainder'))
+        self.pair = pd.DataFrame(columns=['even',
+                                          'shifted_core',
+                                          ],
+                                 index=pd.Index([], name='pair'))
 
     """
     EVENS
     """
 
     def add_even(self, even: EvenSteps):
-        pass
+        self.even.loc[even.even_steps] = [even.odd_steps, even.completed, even.alpha]
 
     def even_exists(self, even: int):
-        pass
+        return even in self.even.index
 
     def even_complete(self, even: int):
-        pass
+        return self.even.at[even, 'completed']
 
     def set_even_completeness(self, even: int, complete: bool):
-        pass
+        self.even.at[even, 'completed'] = complete
 
     def get_even_list(self):
-        pass
+        return list(self.even.index.values)
 
     def get_complete_evens(self, complete: bool):
         pass
